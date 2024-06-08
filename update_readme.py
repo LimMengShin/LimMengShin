f8fe41ce589275a156ba3ada75436004ff4b40f0
Lim Meng Shin <62892957+LimMengShin@users.noreply.github.com>
2024-06-08 11:16:52 +0800

    Create update_readme.py

---
+++ update_readme.py
@@ -0,0 +1,15 @@
+from playwright.sync_api import sync_playwright
+
+def main(): 
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        context = browser.new_context(
+            device_scale_factor=2,
+        )
+        page = context.new_page()
+        page.goto('https://github.com/LimMengShin')
+        page.screenshot(path="screenshot.jpg", type="jpeg", quality=100)
+        context.close()
+
+if __name__ == "__main__":
+    main()
